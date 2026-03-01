7b15d1c080303907ccaba8e85de7246e759f49af
Jayeshs1999 <jayeshsevatkar55@gmail.com>
2026-03-02 00:40:55 +0530

    change navigations

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,8 +94,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
-      { url: "/icon.png", sizes: "32x32", type: "image/png" },
-      { url: "/icon.png", sizes: "16x16", type: "image/png" },
+      { url: "/icon32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon16.png", sizes: "16x16", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
